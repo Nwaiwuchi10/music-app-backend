9b43520f30a039f946d111471e9035c58b912796
Nwaiwuchi10 <djnchrys@gmail.com>
2023-03-13 11:22:15 -0700

    make it better

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ const config = require("config");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mp3Route = require("./routes/mp3");
+const mp4Route = require("./routes/mp4");
 const router = express.Router();
 const app = express();
 app.use(cors());
 app.use(compression());
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(
-  bodyParser.urlencoded({
-    limit: "10mb",
-    extended: true,
-    parameterLimit: 10000,
-  })
-);
+app.use(bodyParser.json({ limit: "20mb" }));
+// app.use(
+//   bodyParser.urlencoded({
+//     limit: "50mb",
+//     extended: true,
+//     parameterLimit: 10000,
+//   })
+// );
 // / create connection
 
 const db = config.get("mongoURI");
@@ -32,11 +33,12 @@ mongoose
     useUnifiedTopology: true,
     // useFindAndModify: false,
   })
-  .then(() => console.log(`MongoDb Connected`))
+  .then(() => console.log(`MongoDb Connected`.bgGreen.bold))
   .catch((err) => console.log(err));
 /////
 app.use(express.json());
 app.use("/api/music", mp3Route);
+app.use("/api/mp4", mp4Route);
 app.use(express.static(path.join(__dirname, "/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "build/index.html"))
---
+++ models/Mp4.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Mp4Schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      max: 50,
+    },
+    genre: {
+      type: String,
+    },
+    recommendSong: {
+      type: Boolean,
+      default: false,
+    },
+
+    artist: {
+      type: String,
+      required: true,
+    },
+    album: { type: String },
+    year: { type: String },
+
+    filepath: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Mp4", Mp4Schema);
