c2e1c3c3be5e1125b3f0cc5b1e46da7fc62ec95a
Nwaiwuchi10 <djnchrys@gmail.com>
2023-07-19 05:01:11 -0700

    build

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
     },
   })
 );
-app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.json({ limit: "150mb" }));
 // app.use(
 //   bodyParser.urlencoded({
 //     limit: "50mb",
